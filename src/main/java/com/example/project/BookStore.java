ab740abb393806a4ed5952edaff83baae0e724a9
Yunarian <167535928+Yunarian@users.noreply.github.com>
2025-01-21 19:53:14 +0000

    Fly, broken wings

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -2,12 +2,12 @@ package com.example.project;
 
 public class BookStore {
 
-    //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
+    // requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
     // static variables
     private static Book[] books = new Book[6];
     private static User[] users = new User[10];
 
-    // empty constructor
+    // constructor
     public BookStore() {
         users = new User[10];
         books = new Book[6];
@@ -22,7 +22,10 @@ public class BookStore {
 
     // adds a user to the first empty element of the users array with the new user
     public void addUser(User user) {
+        // goes through every element of users
         for (int i = 0; i < users.length; i++) {
+            // if that element is null, that element is set equal to the paramter
+            // and the loop ends
             if (users[i] == null) {
                 users[i] = user;
                 break;
@@ -32,9 +35,13 @@ public class BookStore {
 
     // removes the first user which matches the parameter. 
     public void removeUser(User user) {
+        // goes through every element of users
         for (int i = 0; i < users.length; i++) {
+            // if that element isn't null and matches with user
+            // that user is removed, and all users after it are moved foward by 1
             if (users[i] != null && users[i].equals(user)) {
                 users[i] = null;
+                // sends nulls to the end of the array
                 consolidateUsers();
             }
         }
@@ -43,47 +50,33 @@ public class BookStore {
     // modifies the users array such that all empty elements (nulls) are at the back of the array
     public void consolidateUsers() {
         User[] temp = new User[users.length];
-        int index = 0;
+        int currentUserIdx = 0;
+
+        // goes through every element of users
         for (int i = 0; i < users.length; i++) {
+            // if the User object at that element isn't null
+            // it is added to the temporary array
+            // at the first null in the temporary array
             if (users[i] != null) {
-                temp[index] = users[i];
-                index++;
+                temp[currentUserIdx] = users[i];
+                currentUserIdx++;
             }
         }
+        // modifying teh suers array with what ahs been done
         users = temp;
     }
 
     // adds a book to the first empty element in the books array
     public void addBook(Book book){
-        // counts the number of nulls(used later)
-        int count = 0;
-        for (int i = 0; i < books.length; i++){
-            if (books[i] == null){
-                count++;
-            } 
-        }
-
-        Book[] temp = new Book[books.length + 1];
-        // copy the elements of books into temp (temp will have one null at the end)
-        for(int i = 0; i < books.length; i++){
-            temp[i] = books[i];
-        }
-
-        // put book at the end
-        temp[books.length] = book;
-        books = temp;
-
-        // remove extra null values by copying over all non null elements
-        Book[] nullRemover = new Book[books.length - count];
-        count = 0;
-        for(int i = 0; i < books.length; i++ ){
-            if(books[i] != null){
-                nullRemover[count] = books[i];
-                count++;
+        // goes through every element in the books array
+        for (int i = 0; i < books.length; i++) {
+            // if that element is null, that element is set equal to  book
+            // and the loop ends
+            if (books[i] == null) {
+                books[i] = book; 
+                break;
             }
         }
-        // set books to the new array without any nulls
-        books = nullRemover;
     }
 
     // inserts the book at the specified index with the new book
@@ -92,15 +85,19 @@ public class BookStore {
         // then inserts book at index, adding 1 to all books' index after book. 
         Book[] newBooks = new Book[books.length + 1];
         int currentBook = 0;
+        // goes through every element in the newBooks array
         for (int i = 0; i < newBooks.length; i++) {
+            // if not at the specified index, the current book is added to the array
+            // and the current book is incremented by 1
             if (i != index) {
                 newBooks[i] = books[currentBook];
                 currentBook++;
             } else {
+                // if it is at the specified index, that element is set equal to book
                 newBooks[i] = book;
             }
         }
-
+        // modifying the books array with what has been done
         books = newBooks;
     }
 
@@ -114,48 +111,27 @@ public class BookStore {
                 books[i].setQuantity(books[i].getQuantity() - 1);
             }
 
+            // after a book is possibly removed, checks to see if the quantity of that book is less than or equal to 0
+            // if it is, that book gets removed from the books array
             if (books[i].getQuantity() <= 0) {
+                // book is removed, and a new array is made 
+                // with a length 1 less than the length of the array before
                 books[i] = null;
                 Book[] newBooks = new Book[books.length - 1];
-                int count = 0;
+
+                int numDifBooks = 0;
+                // goes through every book in books
                 for (int j = 0; j < books.length; j++) {
+                    // if that element isn't null, it's added to a temporary array
                     if (books[j] != null) {
-                        newBooks[count] = books[j];
-                        count++;
+                        newBooks[numDifBooks] = books[j];
+                        numDifBooks++;
                     }  
                 }
+                // modifying the books array with what has been done
                 books = newBooks;
                 break;  
             }
         }    
     }
-
-    // you are not tested on this method but use it for debugging purposes
-    public String bookStoreUserInfo() {
-        String returnedString = "";
-        for (int i = 0; i < users.length; i++) {
-            if (users[i] != null) {
-              returnedString += users[i].userInfo() + "\n";  
-            } else {
-                returnedString += "NULL\n";
-            }
-            
-            }
-        return returnedString;
-    }
-
-    public String bookStoreBookInfo() {
-        String returnedString = "";
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] != null) {
-                returnedString += books[i].bookInfo() + "\n";  
-            } else {
-                returnedString += "NULL\n";
-            }
-            
-            }
-        return returnedString;
-    }
- 
-
 }
